9bc4376b7d264520be8608f17715164f360317fc
winnier21 <you@example.com>
2024-07-07 15:04:44 -0700

    navbar styling hover states updated

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,19 @@
 import "./NavBar.scss";
-import logo from "../../assets/icons/logo.png";
-import call from "../../assets/icons/emergency_call2.svg";
+import call from "../../assets/icons/emergency_call.svg";
 import navigation from "../../assets/icons/navigation.svg";
 // import React from "react";
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <img className="navbar__logo" alt="logo" src={logo} />
       <section className="navbar__section">
         <div className="navbar__item">
           <img className="navbar__call" alt="emergency call" src={call} />
-          <h3>Emergency Call</h3>
-        </div>
-        <div className="navbar__item">
+          <h3 className="navbar__text">Emergency Call</h3>
+          </div>
+          <div className="navbar__item">
           <img className="navbar__navigation" alt="navigation" src={navigation} />
-          <h3>Navigation</h3>
+          <h3 className="navbar__text">Navigation</h3>
         </div>
       </section>
     </nav>
