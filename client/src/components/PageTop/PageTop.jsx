20b7aac6d56d23b01760f07d6c0dc38a65847bc9
winnier21 <you@example.com>
2024-07-07 21:15:04 -0700

    vector icon and slogan updated

--- client/src/components/PageTop/PageTop.jsx
+++ client/src/components/PageTop/PageTop.jsx
@@ -1,5 +1,6 @@
 import logo from "../../assets/icons/logo.png";
 import SwipeButton from "../button/SwipeButton/SwipeButton";
+import vectorIcon from "../../assets/icons/vector.svg";
 import "./PageTop.scss";
 
 function PageTop() {
@@ -10,10 +11,15 @@ function PageTop() {
       </section>
       <section className="pagetop-section">
         <h1>STEP 1</h1>
-        <h3 className="pagetop-text">Stay Clam. Don't Wait, act now</h3>
+        <h3>Stay Clam. Don't Wait, act now</h3>
         <SwipeButton />
       </section>
+      <article className="pagetop-container">
+      <img className="pagetop-vector" alt="vector icon" src={vectorIcon}></img>
+      <h3 className="pagetop-text">This app will automatically dial the next closest clinic if this call fails. </h3>
+      </article>
     </main>
+    
   );
 }
 export default PageTop;
